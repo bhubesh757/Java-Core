502eaf3cd60b10e56b5b64d5fd8b8a9aadea0aeb
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-07-14 21:49:32 +0530

    Create Main.java

---
+++ Day-1/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+//    why main is static?
+
+
+    public static void main(String[] args) {
+
+//        Creating a object
+//        Employee e = new Employee();
+//        e.data=34; //public int is from the Employee.java
+
+        Employee e1 = new Employee();
+        e1.data=44;
+
+        Employee e = new Employee();
+       e.doSomething();
+
+//        System.out.println(e.data);
+//        System.out.println(e1.data);
+        
+
+
+    }
+}
