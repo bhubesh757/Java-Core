fb623649ca00ac129c34b513d9a39e142d6062f7
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-07-14 21:49:50 +0530

    Create Employee.java

---
+++ Day-1/Employee.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Employee {
+//    Its Public and its available everywhere in the project
+    public int data;
+
+    String name = "bhubesh";
+
+//    private => its only accessible only inside the class
+//    default => it works for a(only one )  package
+
+    //        Function inside a class
+
+//    function always returns
+//    specify the return datatype
+//    public int doSomething(){
+//        System.out.println("Bhubesh");
+//        return 1;
+//    }
+public void doSomething(){
+    System.out.println(name);
+
+}
+
+//    Strings
+//    Strings are class
+//    String are Objects
+//    Example :
+//    String name = new String(original:"Bhubesh") this works in back
+
+
+//    To Doesnt want to return anything if we use void in function()
+
+
+
+}
