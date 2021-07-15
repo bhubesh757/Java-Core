699f414a27e7d2991ffa0cb0773c1ef1d03fe212
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-07-15 08:12:20 +0530

    Create Concatination_and_takinginputfromtheuser.java

---
+++ Concatination_and_takinginputfromtheuser.java
@@ -0,0 +1,13 @@
+String name = "SpiderMan";
+        int age = 47;
+
+        System.out.println("The name is :" +name );
+        System.out.println(name+"'s age is :" +age );
+
+        System.out.println("Enter the name");
+        String name = sc.next();
+        System.out.println("enter the age");
+        int age = sc.nextInt();
+
+        System.out.println(name);
+        System.out.println(age);
