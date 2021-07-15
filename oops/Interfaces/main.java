789ddc5154c4dcd7c472533197ece8d8b770f544
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-07-15 22:24:43 +0530

    Create main.java

---
+++ oops/Interfaces/main.java
@@ -0,0 +1,82 @@
+package com.company;
+
+//Scanner package
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Circle c = new Circle();
+        c.area();
+
+        Square s = new Square();
+        s.area();
+
+//        Teacher t = new Teacher();
+//        t.getDetails();
+//        t.displayDetails();
+//        t.markAttendance();
+
+
+//        interface! it uses the implements keyword
+//        its also like a class
+//        if you create a property inside a interface it is a final means constant
+//        constants are known as final!
+//        What is abstract method ?
+//        method();
+
+
+//        Inheritance
+//        the process of extending the features(the parameters ...) of one class by another class!
+//        syntax
+//        This is the class one
+//        class demo{
+//            prop1 , prop2 , prop3;
+//            private prop6;
+//            protected prop7
+//
+////            private things are not inherited!
+////            protected can be used by current class and the child class!
+//
+//
+//            method1();
+//            method2();
+//            method3();
+//
+//        }
+//
+////        class 2
+//
+//        class NewClass extends Demo
+//        {
+//            prop5 , prop4;
+//
+//            method3();
+//            method4();
+//
+//        }
+
+
+
+//
+////        constructor?
+////        the classname and function or contructor names are always same
+//        Employee e = new Employee();
+//
+////        e.getDetails();
+////        Here we are trying to do the data hiding!
+//
+////        e.name = "Bhubesh";
+////        e.displayDetails();
+//
+//        e.setName("Bhubesh");
+//        e.setAge(45);
+//        System.out.println(e.getAge());
+//        System.out.println(e.getName());
+
+    }
+}
+
+
