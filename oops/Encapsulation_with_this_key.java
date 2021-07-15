086dd73475946ac04951663bad5b56cc9acad10c
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-07-15 17:53:55 +0530

    Create Encapsulation_with_this_key.java

---
+++ oops/Encapsulation_with_this_key.java
@@ -0,0 +1,29 @@
+package com.company;
+
+//Scanner package
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        constructor?
+//        the classname and function or contructor names are always same
+        Employee e = new Employee();
+
+//        e.getDetails();
+//        Here we are trying to do the data hiding!
+
+//        e.name = "Bhubesh";
+//        e.displayDetails();
+
+        e.setName("Bhubesh");
+        e.setAge(45);
+        System.out.println(e.getAge());
+        System.out.println(e.getName());
+
+    }
+}
+
+
