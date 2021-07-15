bce7447c1f836682a2a0a8279c72088c1f1d918d
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-07-15 07:20:13 +0530

    Create Conditional_Statements.java

---
+++ Conditional_Statements.java
@@ -0,0 +1,32 @@
+package com.company;
+
+//Scanner package
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int num = 2;
+
+//        Switch Statement
+        switch (num) {
+            case 2: {
+                System.out.println("I am Two");
+                break; //It breaks the statement;
+            }
+            case 4: {
+                System.out.println("I am Three");
+                break;
+            }
+            default: {
+                System.out.println("All the cases are false");
+                break;
+            }
+        }
+
+
+    }
+
+}
